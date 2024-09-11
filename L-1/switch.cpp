be9b818b2b41453f5f61ddecff0933a8c955a026
codewithswatiee <verma.s@somaiya.edu>
2024-09-11 06:37:13 +0530

    startes DSA on 10th Sep

---
+++ L-1/switch.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int day;
+    cin >> day;
+    switch(day){
+        case 1:
+            cout << "Mon";
+            break;
+        case 2:
+            cout << "Tue";
+            break;
+        case 3:
+            cout << "Wed";
+            break;
+        case 4:
+            cout << "Thu";
+            break;
+        case 5:
+            cout << "Fri";
+            break;
+        case 6:
+            cout << "Sat";
+            break;
+        case 7:
+            cout << "Sun";
+            break;
+        default:
+            cout << "Invalid day";
+    }
+    return 0;
+}
---
+++ L-1/while.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i = 0;
+    while(i < 5){
+        cout << i << endl;
+        i++;
+    }
+}
