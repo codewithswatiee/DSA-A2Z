b87802e5e9be7f3c5bc555a424b0ee7988c3fc4e
codewithswatiee <verma.s@somaiya.edu>
2024-09-24 10:50:14 +0530

    made changes

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -11,7 +11,7 @@ void bubbleSort(vector<int>& arr) {
     int n = arr.size();
     for(int i = n - 1; i > 0; i--){
         int didSwap = 0;
-        for(int j = 0; j <= n - 1; j++){
+        for(int j = 0; j < i ; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr, j, j+1);
                 didSwap = 1;
