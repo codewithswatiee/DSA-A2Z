a4defd9ce5d9e1b0bd973ed41328a83ebc2530bc
codewithswatiee <verma.s@somaiya.edu>
2024-09-24 10:23:04 +0530

    sorting-1 completed

--- sorting/insertionSort.cpp
+++ sorting/insertionSort.cpp
@@ -7,25 +7,20 @@ void swap(vector<int>& arr, int a, int b) {
     arr[b] = temp;
 }
 
-void bubbleSort(vector<int>& arr) {
+void insertionSort(vector<int>& arr) {
     int n = arr.size();
-    for(int i = n - 1; i > 0; i--){
-        int didSwap = 0;
-        for(int j = 0; j <= n - 1; j++){
-            if(arr[j] > arr[j+1]){
-                swap(arr, j, j+1);
-                didSwap = 1;
-            }
-        }
-        if(didSwap == 0){
-            break;
+    for(int i = 0; i < n; i++){
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            swap(arr, j, j-1);
+            j--;
         }
     }
 }
 
 int main() {
     vector<int> arr = {64, 25, 12, 22, 11};
-    bubbleSort(arr);
+    insertionSort(arr);
     for (auto el : arr) {
         cout << el << endl; 
     }
