20293f14f52382567e2e46129cc74673a2837aed
codewithswatiee <verma.s@somaiya.edu>
2024-09-27 18:00:15 +0530

    "Added comments to explain the approach and functionality of the bubbleSort algorithm"

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// understanding the approach here:
+/*
+    First we created a swap function to swap the elements of the array.
+    Then we used the swap function to swap the elements of the array in the BubbleSort function.
+    Now, In the bubbleSort function we will take array as the input.
+*/
 void swap(vector<int>& arr, int a, int b) {
     int temp = arr[a];
     arr[a] = arr[b];
     arr[b] = temp;
 }
 
+
+/*
+    Explaining bubbleSort function:
+    n is the size of array.
+    the first loop indicates till where the elements have been sorted and the inner loop loops over till i (i.e element just before the now sorted array.)
+    didSwap is the flag which is used so that if whenever the swap doesn't happen the loop ends to reduce the time complexity of the program.
+*/
 void bubbleSort(vector<int>& arr) {
     int n = arr.size();
     for(int i = n - 1; i > 0; i--){
