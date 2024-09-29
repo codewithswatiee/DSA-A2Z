5aa50b2f1ca62d8490c049854d2773e9b62c1cc6
codewithswatiee <verma.s@somaiya.edu>
2024-09-29 23:21:53 +0530

    "Refactored rotate function in rotate.cpp to use reverse function for array rotation"

---
+++ arrays/moveZero.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(vector<int> &arr, int a, int b){
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+void moveZero(vector<int> &arr){
+    int n = arr.size();
+    int j = -1;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    for(int i = j+1; i < n; i++){
+        if(arr[i] != 0){
+            swap(arr, i, j);
+            j++;
+        }
+    }
+
+}
+
+int main(){
+    vector<int> arr = {0, 1, 0, 3, 12};
+    moveZero(arr);
+    cout << "Array after moving zeros: ";
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+
+}
--- arrays/rotate.cpp
+++ arrays/rotate.cpp
@@ -1,16 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rotate(vector<int> &arr,int k){
-    int n = arr.size();
-    while(k > 0){
-        int temp = arr[n - 1];
-        for(int i = n - 1; i > 0; i--){
-            arr[i] = arr[i - 1];
-        }
-        arr[0] = temp;
-        k--;
+
+//Brute Force
+// void rotate(vector<int> &arr,int k){
+//     int n = arr.size();
+//     while(k > 0){
+//         int temp = arr[n - 1];
+//         for(int i = n - 1; i > 0; i--){
+//             arr[i] = arr[i - 1];
+//         }
+//         arr[0] = temp;
+//         k--;
+//     }
+// }
+
+void reverse(vector<int> &arr, int start, int end){
+    while(start <= end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
+}
+
+void rotate(vector<int> &arr, int k){
+    int n = arr.size();
+    k = k % n;
+    reverse(arr, 0, n - k - 1);
+    reverse(arr, n - k, n - 1);
+    reverse(arr, 0, n-1);
 }
 
 int main(){
